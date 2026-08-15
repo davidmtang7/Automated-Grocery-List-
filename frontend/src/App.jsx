befed3a6641df21b9bb9d11fa3c54a6e59f1a9b5
David Tang <dtang46435@gmail.com>
2026-08-15 22:15:28 +0700

    Ingredient list is displayable

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,29 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import { useEffect } from 'react';
-
+function IngredientList(){
+  const[ingredients, setIngredients] = useState([])
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/ingredients')
+      .then(response => response.json())
+      .then(data => setIngredients(data))
+  }, []);
+  return(
+    <div>
+      {ingredients.map(ingredient => (
+        <p key={ingredient.id}>{ingredient.name}:  ${ingredient.price}</p>
+      ))}
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
       <h1>Ingredients list</h1>
-      <button>
-        Display ingredients list
-      </button>
+      <IngredientList>
+
+      </IngredientList>
     </div>
   );
 }
